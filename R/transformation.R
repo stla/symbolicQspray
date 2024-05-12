dfc0821957be241fc754ea579a64d1904a9373d1
stla <laurent_step@yahoo.fr>
2024-05-12 21:04:55 +0200

    modif changeVariables

--- R/transformation.R
+++ R/transformation.R
@@ -231,6 +231,12 @@ setMethod(
       }
       result <- result + coeffs[[i]] * term
     }
+    notSymbolicQsprays <- all(vapply(listOfQsprays, function(x) {
+      !inherits(x, "symbolicQspray")
+    }, logical(1L)))
+    if(notSymbolicQsprays) {
+      return(getConstantTerm(result))
+    }
     sSQ <- getShowSymbolicQspray(x)
     showSymbolicQsprayOption(result, "showRatioOfQsprays") <-
       attr(sSQ, "showRatioOfQsprays")
